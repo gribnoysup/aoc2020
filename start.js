0036b7d4fcbc153d0b6686bde5ddc49726ea8df5
Sergey Petushkov <petushkov.sergey@gmail.com>
2020-12-02 18:52:55 +0100

    index.js -> start.js

--- start.js
+++ start.js
@@ -1,38 +1,52 @@
+import path from "path";
 import fs from "fs/promises";
 import inquirer from "inquirer";
-
-console.log("Advent of Code 2020 Solutions Sandbox");
+import { fileURLToPath } from "url";
 
 console.log();
+console.log("Advent of Code 2020 Solutions Sandbox");
+console.log();
 
-const dirs = (await fs.readdir(process.cwd())).filter((dirName) =>
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const dirs = (await fs.readdir(__dirname)).filter((dirName) =>
   /^day/.test(dirName)
 );
+
+const choices = dirs.map((dir_name) => {
+  return { name: `Day ${dir_name.replace("day", "")}`, value: dir_name };
+});
 
 const { solution_dir } = await inquirer.prompt([
   {
     type: "list",
     name: "solution_dir",
     message: "Choose a day to run",
-    choices: dirs.map((dir_name) => {
-      return { name: `Day ${dir_name.replace("day", "")}`, value: dir_name };
-    }),
+    choices,
+    default: choices[choices.length - 1].value,
   },
 ]);
 
 console.log();
 
 let solution;
+let parser = (_) => _;
 let input;
 
 try {
   solution = await import(`./${solution_dir}/index.js`);
-  input = await fs.readFile(`./${solution_dir}/input.txt`, "utf8");
+  parser = solution?.parse_input ?? parser;
+  input = parser(await fs.readFile(`./${solution_dir}/input.txt`, "utf8"));
 } catch {}
+
+if (!input) {
+  console.warn("⚠️  No input provided for the solutions");
+  console.log();
+}
 
 const timeLabel = "Time".padStart("Part 1 Answer".length);
 
-if (solution && solution.part1) {
+if (solution?.part1) {
   console.time(timeLabel);
   console.log("Part 1 Answer:", await solution.part1(input));
   console.timeEnd(timeLabel);
@@ -42,7 +56,7 @@ if (solution && solution.part1) {
 
 console.log();
 
-if (solution && solution.part2) {
+if (solution?.part2) {
   console.time(timeLabel);
   console.log("Part 2 Answer:", await solution.part2(input));
   console.timeEnd(timeLabel);
