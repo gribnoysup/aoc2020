cc900a02fae5d311ebebdf8fa2dd8cd850243572
Sergey Petushkov <petushkov.sergey@gmail.com>
2020-12-03 22:53:40 +0100

    Add day 3 solution

---
+++ day3/index.js
@@ -0,0 +1,61 @@
+/**
+ * @param {string} input
+ */
+export function parse_input(input) {
+  return input
+    .trim()
+    .replaceAll(".", "0")
+    .replaceAll("#", "1")
+    .split("\n")
+    .map((line) => {
+      return line.split("").map(Number);
+    });
+}
+
+function count_with_a_step(arr, x, y) {
+  const line_length = arr[0].length;
+  let res = 0;
+  let j = 0;
+  for (let i = 0; i < arr.length; i += x) {
+    res += arr[i][j];
+    j = (j + y) % line_length;
+  }
+  return res;
+}
+
+/**
+ * @param {ReturnType<typeof parse_input>} forest_map
+ */
+export function part1(forest_map) {
+  return count_with_a_step(forest_map, 1, 3);
+}
+
+function one_pass_count(arr, steps) {
+  const line_length = arr[0].length;
+  const results = Array(steps.length).fill(0);
+  const curr_step_y = Array(steps.length).fill(0);
+  for (let i = 0; i < arr.length; i++) {
+    steps.forEach(([x, y], s) => {
+      if (s % x === 0) {
+        results[s] += arr[i][curr_step_y[s]];
+        curr_step_y[s] = (curr_step_y[s] + y) % line_length;
+      }
+    });
+  }
+  return results;
+}
+
+/**
+ * @param {ReturnType<typeof parse_input>} forest_map
+ */
+export function part2(forest_map) {
+  const steps = [
+    [1, 1],
+    [1, 3],
+    [1, 5],
+    [1, 7],
+    [2, 1],
+  ];
+
+  return one_pass_count(forest_map, steps).reduce((a, b) => a * b, 1);
+}
