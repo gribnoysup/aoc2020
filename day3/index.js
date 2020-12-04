0fe6b0f3c49d81802107c80ac6f988e003402d2f
Sergey Petushkov <petushkov.sergey@gmail.com>
2020-12-04 19:13:47 +0100

    Refactor day 3 to operate on a 1d array

--- day3/index.js
+++ day3/index.js
@@ -2,6 +2,23 @@
  * @param {string} input
  */
 export function parse_input(input) {
+  return {
+    length: input.indexOf("\n"),
+    arr: input
+      .trim()
+      .replaceAll(".", "0")
+      .replaceAll("#", "1")
+      .replaceAll("\n", "")
+      .split("")
+      .map(Number),
+  };
+}
+
+/**
+ * @deprecated
+ * @param {string} input
+ */
+function to_2d_array(input) {
   return input
     .trim()
     .replaceAll(".", "0")
@@ -13,9 +30,44 @@ export function parse_input(input) {
 }
 
 /**
+ *
+ * @param {number[]} arr
+ * @param {number} x
+ * @param {number} y
+ */
+function get_steps_for_path({ arr, length }, x, y) {
+  x = x * length;
+  let curr_pos = 0;
+  const max_step = x + length;
+  const step = x + y;
+  const steps = [curr_pos];
+
+  while (true) {
+    const next_pos =
+      curr_pos + step - Number((curr_pos % length) + step >= max_step) * length;
+
+    if (next_pos >= arr.length) {
+      break;
+    }
+
+    steps.push(next_pos);
+    curr_pos = next_pos;
+  }
+
+  return steps;
+}
+
+function count_trees_on_path({ arr }, steps) {
+  return steps.reduce((acc, step) => {
+    return acc + arr[step];
+  }, 0);
+}
+
+/**
+ * @deprecated
  * @param {number[][]} arr 2d array representing the "trees"
- * @param {number} x vertical move distance
- * @param {number} y horizontal move distance
+ * @param {number} x "vertical" move distance
+ * @param {number} y "horizontal" move distance
  * @returns {number}
  */
 function count_with_a_step(arr, x, y) {
@@ -32,11 +84,12 @@ function count_with_a_step(arr, x, y) {
 /**
  * @param {ReturnType<typeof parse_input>} forest_map
  */
-export function part1(forest_map) {
-  return count_with_a_step(forest_map, 1, 3);
+export function part1(forest) {
+  return count_trees_on_path(forest, get_steps_for_path(forest, 1, 3));
 }
 
 /**
+ * @deprecated
  * @param {number[][]} arr 2d array representing the "trees"
  * @param {[number, number][]} steps array of step variants that we are checking
  * @returns {number[]}
@@ -59,7 +112,7 @@ function one_pass_count(arr, steps) {
 /**
  * @param {ReturnType<typeof parse_input>} forest_map
  */
-export function part2(forest_map) {
+export function part2(forest) {
   const steps = [
     [1, 1],
     [1, 3],
@@ -68,5 +121,9 @@ export function part2(forest_map) {
     [2, 1],
   ];
 
-  return one_pass_count(forest_map, steps).reduce((a, b) => a * b, 1);
+  return steps
+    .map(([x, y]) => {
+      return count_trees_on_path(forest, get_steps_for_path(forest, x, y));
+    })
+    .reduce((a, b) => a * b, 1);
 }
