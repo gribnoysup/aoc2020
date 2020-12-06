b28e7e46ecc240bb76009cc820fdda687bc519da
Sergey Petushkov <petushkov.sergey@gmail.com>
2020-12-06 09:57:17 +0100

    Finish early when solution for the day already exists

--- create.js
+++ create.js
@@ -3,6 +3,12 @@ import fs from "fs/promises";
 import { fileURLToPath } from "url";
 import inquirer from "inquirer";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+function get_folder_name(day) {
+  return `day${String(day)}`;
+}
+
 const js_template = `
 /**
  * @param {string} input
@@ -29,42 +35,47 @@ console.log();
 console.log("Advent of Code 2020 Solution Template Creator");
 console.log();
 
-const { day, sample_data, input_data } = await inquirer.prompt([
+const { day } = await inquirer.prompt([
   {
     type: "number",
     name: "day",
     message: "Enter day number",
   },
-  {
-    type: "editor",
-    name: "sample_data",
-    message: "Enter sample data",
-  },
-  {
-    type: "editor",
-    name: "input_data",
-    message: "Enter puzzle input",
-  },
 ]);
 
-const folder = `day${String(day)}`;
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const solution_folder = path.resolve(__dirname, folder);
+const folder = get_folder_name(day);
+const solution_dir = path.resolve(__dirname, folder);
 
 try {
-  await fs.mkdir(solution_folder);
-  await fs.writeFile(path.join(solution_folder, "sample.txt"), sample_data);
-  await fs.writeFile(path.join(solution_folder, "input.txt"), input_data);
-  await fs.writeFile(path.join(solution_folder, "index.js"), js_template);
+  await fs.stat(solution_dir);
 
-  console.log();
-  console.log("All done. Enjoy!");
-  console.log();
-} catch (err) {
   console.warn();
   console.warn(`⚠️  Solution for day ${day} already exists!`);
   console.warn();
 
   process.exitCode = 1;
+} catch {
+  // ENOENT, so all good, continue!
+
+  const { sample_data, input_data } = await inquirer.prompt([
+    {
+      type: "editor",
+      name: "sample_data",
+      message: "Enter sample data",
+    },
+    {
+      type: "editor",
+      name: "input_data",
+      message: "Enter puzzle input",
+    },
+  ]);
+
+  await fs.mkdir(solution_dir);
+  await fs.writeFile(path.join(solution_dir, "sample.txt"), sample_data);
+  await fs.writeFile(path.join(solution_dir, "input.txt"), input_data);
+  await fs.writeFile(path.join(solution_dir, "index.js"), js_template);
+
+  console.log();
+  console.log("All done. Enjoy!");
+  console.log();
 }
